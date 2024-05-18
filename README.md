27c7954643a48f12a457936d034e9f58cd66de7b
Martin Monperrus <martin.monperrus@gnieh.org>
2024-05-18 12:38:57 +0200

    up

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # openaiisrich
-openai is rich -- pricing information of openai as JSON
+
+openai is rich -- pricing information of openai as JSON.
+
+pull requests welcome.
+
+## Usage
+
+### Usage in Python
+
+Install with `pip install openaiisrich`
+
+Then
+
+```python
+import openaiisrich
+
+print("cost ", openaiisrich.cost("gpt-4", 507, 2987))
+
+```
+
+### Usage in Javascript
+
+TODO
+
+## License
+
+MIT
---
+++ __init__.py
@@ -0,0 +1,13 @@
+import openaiisrich
+import re
+import json
+from importlib import resources 
+    
+def load_json():
+  return json.loads(resources.read_text(openaiisrich,"openai-pricing.json"))
+
+DATA = load_json()
+
+def cost(model, n_prompt_tokens, n_completion_tokens):
+  model_pricing = DATA["model"]
+  return n_prompt_tokens/model_pricing["per_tokens"]*model_pricing["prompt"] + n_completion_tokens/model_pricing["per_tokens"]*model_pricing["completion"]
